d59d4a2311a3c2ab242348e5583f86d23dd5bf00
pkhaleelb <pkbasha143@gmail.com>
2025-01-22 17:30:06 +0530

    Create method.py

---
+++ method.py
@@ -0,0 +1,23 @@
+# class maths:
+#     def __init__(self):
+#         self.sub_name='maths'
+#     def add(self):
+#         a,b=10,20
+#         print(a+b)
+# m1=maths()
+# m1.add()
+
+# class Instagram:
+#     def __init__(self):
+#         self.version='13.1'
+#         self.developer='khaleel'
+#     def auth(self):
+#         user_name=input('enter user name:')
+#         password=input('enter password:')
+#         if user_name=='prince123'and password=='1234':
+#             print('successful')
+#         else:
+#             print('login failed')
+# im=Instagram()
+# im.auth()
+        
